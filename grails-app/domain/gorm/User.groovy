ce940d12425821333288e0509d739bae9e00ee29
[david] <[darubiob@unal.edu.co]>
2015-10-24 18:28:39 -0500

    Class User

--- grails-app/domain/gorm/User.groovy
+++ grails-app/domain/gorm/User.groovy
@@ -1,7 +1,22 @@
 package gorm
 
 class User {
+    String name
+    String lastname
+    int age
+    String username
+    String password
+
+
 
     static constraints = {
+        name(blank:false, minSize: 3, maxSize: 50)
+        lastname(blank:false, minSize: 3, maxSize: 50)
+        age(nullable: false, min: 13)
+        username(blank:false, unique:true)
+        password(blank:false, matches: /(\w*([A-Z])+\w*([0-9])+\w*)|(\w*([0-9])+\w*([A-Z])+\w*)/)
+
     }
+
+
 }
