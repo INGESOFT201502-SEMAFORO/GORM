d7f266584a78372cecc57c94523ed9b342374708
Juan Pablo Arias <jpariasc@unal.edu.co>
2015-10-31 18:50:26 -0500

    controllers Admin - Regular

--- grails-app/controllers/gorm/AdminController.groovy
+++ grails-app/controllers/gorm/AdminController.groovy
@@ -1,6 +1,112 @@
 package gorm
 
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
+
+@Transactional(readOnly = true)
 class AdminController {
 
-    def index() {}
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        render Admin.list()
+    }
+
+    def show(Admin adminInstance) {
+        respond adminInstance
+    }
+
+    def create() {
+        respond new Admin(params)
+    }
+
+    @Transactional
+    def save(Admin adminInstance) {
+        if (adminInstance == null) {
+            notFound()
+            return
+        }
+
+        if (adminInstance.hasErrors()) {
+            println adminInstance.errors
+            return
+        }
+
+        adminInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'admin.label', default: 'Admin'), adminInstance.id])
+                redirect adminInstance
+            }
+            '*' { respond adminInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Admin adminInstance) {
+        respond adminInstance
+    }
+
+    @Transactional
+    def update(Admin adminInstance) {
+        if (adminInstance == null) {
+            notFound()
+            return
+        }
+
+        if (adminInstance.hasErrors()) {
+            println adminInstance.errors
+            respond adminInstance.errors, view:'edit'
+            return
+        }
+
+        adminInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Admin.label', default: 'Admin'), adminInstance.id])
+                redirect adminInstance
+            }
+            '*'{ respond adminInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Admin adminInstance) {
+
+        if (adminInstance == null) {
+            notFound()
+            return
+        }
+
+        adminInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Admin.label', default: 'Admin'), adminInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        println 'Admin instance with id = $params.id not found'
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'admin.label', default: 'Admin'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+
+    def beforeInterceptor = {
+        println "Se va a ejecutar la accion: $actionName"
+    }
+
+    def afterInterceptor = {
+        println "Se ha ejecutado la accion: $actionName"
+    }
+
 }
--- grails-app/controllers/gorm/RegularController.groovy
+++ grails-app/controllers/gorm/RegularController.groovy
@@ -1,6 +1,110 @@
 package gorm
+import static org.springframework.http.HttpStatus.*
+import grails.transaction.Transactional
 
+@Transactional(readOnly = true)
 class RegularController {
 
-    def index() {}
+    static allowedMethods = [save: "POST", update: "PUT", delete: "DELETE"]
+
+    def index(Integer max) {
+        render Regular.list()
+    }
+
+    def show(Regular regularInstance) {
+        respond regularInstance
+    }
+
+    def create() {
+        respond new Regular(params)
+    }
+
+    @Transactional
+    def save(Regular regularInstance) {
+        if (regularInstance == null) {
+            notFound()
+            return
+        }
+
+        if (regularInstance.hasErrors()) {
+            println regularInstance.errors
+            return
+        }
+
+        regularInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.created.message', args: [message(code: 'regular.label', default: 'Regular'), regularInstance.id])
+                redirect regularInstance
+            }
+            '*' { respond regularInstance, [status: CREATED] }
+        }
+    }
+
+    def edit(Regular regularInstance) {
+        respond regularInstance
+    }
+
+    @Transactional
+    def update(Regular regularInstance) {
+        if (regularInstance == null) {
+            notFound()
+            return
+        }
+
+        if (regularInstance.hasErrors()) {
+            respond regularInstance.errors, view:'edit'
+            return
+        }
+
+        regularInstance.save flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.updated.message', args: [message(code: 'Regular.label', default: 'Regular'), regularInstance.id])
+                redirect regularInstance
+            }
+            '*'{ respond regularInstance, [status: OK] }
+        }
+    }
+
+    @Transactional
+    def delete(Regular regularInstance) {
+
+        if (regularInstance == null) {
+            notFound()
+            return
+        }
+
+        regularInstance.delete flush:true
+
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.deleted.message', args: [message(code: 'Regular.label', default: 'Regular'), regularInstance.id])
+                redirect action:"index", method:"GET"
+            }
+            '*'{ render status: NO_CONTENT }
+        }
+    }
+
+    protected void notFound() {
+        println 'Regular instance with id = $params.id not found'
+        request.withFormat {
+            form multipartForm {
+                flash.message = message(code: 'default.not.found.message', args: [message(code: 'regular.label', default: 'Regular'), params.id])
+                redirect action: "index", method: "GET"
+            }
+            '*'{ render status: NOT_FOUND }
+        }
+    }
+
+    def beforeInterceptor = {
+        println "Se va a ejecutar la accion: $actionName"
+    }
+
+    def afterInterceptor = {
+        println "Se ha ejecutado la accion: $actionName"
+    }
+
 }
